c123003644ed48d70a0329588fa5ebc40ae40853
Matt Daniels <matthew.f.daniels@gmail.com>
2020-07-25 15:48:51 -0400

    svelting

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,23 +1,69 @@
 <style>
-  /* vars defined in public/assets/global.css */
+
+  @font-face {
+  	font-family: 'Lyon Display Web';
+  	src: url('./../assets/fonts/lyon/LyonDisplay-Regular-Web.woff2') format('woff2'),
+  	url('./../assets/fonts/lyon/LyonDisplay-Regular-Web.woff') format('woff');
+  	font-weight: 300;
+  	font-style: normal;
+  	font-stretch: normal;
+  	font-display: swap;
+  }
+  @font-face {
+  	font-family: 'Lyon Text Web';
+  	src: url('./../assets/fonts/lyon/LyonText-Regular-Web.woff2') format('woff2'),
+  	url('./../assets/fonts/lyon/LyonText-Regular-Web.woff') format('woff');
+  	font-weight: 300;
+  	font-style: normal;
+  	font-stretch: normal;
+  	font-display: swap;
+  }
+
+  p {
+    text-align: left;
+    font-family: 'Lyon Text Web';
+    margin: 0 auto;
+    margin-bottom: 1.5rem;
+    line-height: 1.4;
+    font-size: 1.4rem;
+    color: rgba(0,0,0,.8);
+
+  }
+
   h1 {
-    color: var(--c1);
+    font-size: 6rem;
+    font-family: 'Lyon Display Web', sans-serif;
+  }
+
+  svg {
+    display: block;
+    width: 10rem;
   }
 
   strong {
     color: var(--c2);
   }
 
+  .intro {
+    max-width: 500px;
+    margin: 0 auto;
+  }
+
   img {
     width: 20rem;
     display: block;
   }
+
   .chart {
     position: relative;
     width: 20rem;
     height: 20rem;
     background: pink;
   }
+
+
+
+
 </style>
 
 <script>
@@ -29,6 +75,7 @@
   import Scatter from "./Scatter.svelte";
   import petData from "../data/pets.csv";
   import user from "tabler-icons/icons/user.svg";
+  import Meta from "./Meta.svelte";
   import doc from "../data/copy.json"
 
   let name = "Loading";
@@ -41,11 +88,20 @@
   onMount(() => (name = "User"));
 </script>
 
-{#each doc.intro as intro}
-  <p class='prose'>{intro.value}</p>
-{/each}
+<svelte:head>
+  <Meta />
+</svelte:head>
 
-<h1>Hello {name}!</h1>
+<h1>{doc.hed}</h1>
+
+<main id="main">
+  <div class="intro">
+    {#each doc.intro as intro}
+      <p class='prose'>{@html intro.value}</p>
+    {/each}
+  </div>
+</main>
+
 
 {#each petData as { name, pet }}
   <p>
---
+++ src/components/Meta.svelte
@@ -0,0 +1,54 @@
+<!-- <title>{$copy.title[0].text}</title>
+<meta name="description" content="{$copy.description[0].text}" />
+<meta name="news_keywords" content="{$meta.keywords}" />
+
+<meta property="og:title" content="{$copy.title[0].text}" />
+<meta property="og:site_name" content="{$meta.siteName}" />
+<meta property="og:url" content="{$meta.url}" />
+<meta property="og:description" content="{$copy.description[0].text}" />
+<meta property="og:type" content="article" />
+<meta property="og:locale" content="en_US" />
+
+<meta property="og:image" content="{$meta.imgFacebook}" />
+<meta property="og:image:type" content="image/jpeg" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="600" />
+
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="{$meta.site}" />
+<meta name="twitter:creator" content="{$meta.author.twitterHandle}" />
+<meta name="twitter:title" content="{$copy.title[0].text}" />
+<meta name="twitter:description" content="{$copy.description[0].text}" />
+<meta name="twitter:image:src" content="{$meta.imgTwitter}" />
+
+<link rel="canonical" href="{$meta.url}" /> -->
+
+<link rel='preload' type='font/woff2' as='font' crossorigin href='./../assets/fonts/lyon/LyonText-Regular-Web.woff2'>
+<link rel='preload' type='font/woff2' as='font' crossorigin href='./../assets/fonts/lyon/LyonDisplay-Regular-Web.woff2'>
+
+<link
+  rel="apple-touch-icon"
+  sizes="180x180"
+  href="https://pudding.cool/apple-touch-icon.png" />
+
+<link
+  rel="icon"
+  type="image/png"
+  sizes="32x32"
+  href="https://pudding.cool/favicon-32x32.png" />
+
+<link
+  rel="icon"
+  type="image/png"
+  sizes="16x16"
+  href="https://pudding.cool/favicon-16x16.png" />
+
+<link rel="manifest" href="https://pudding.cool/site.webmanifest" />
+
+<link
+  rel="mask-icon"
+  href="https://pudding.cool/safari-pinned-tab.svg"
+  color="#5bbad5" />
+
+<meta name="msapplication-TileColor" content="#ffc40d" />
+<meta name="theme-color" content="#ffffff" />
