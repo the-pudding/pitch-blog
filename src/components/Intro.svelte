35874b4cd4475fa4477e0d5f193ae9b126a359fb
Matt Daniels <matthew.f.daniels@gmail.com>
2020-07-27 10:46:49 -0400

    blog post finished

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -23,25 +23,47 @@
   h3, h1 {
     font-family: 'Lyon Text Web';
     text-align: center;
+
   }
 
   h3 {
+    width: 95%;
     margin-bottom: 1rem;
     text-transform: uppercase;
     letter-spacing: 2px;
     color: #bd3b32;
-    font-size: 1.5rem;
+
+    font-size: 1.3rem;
     margin-top:4rem;
   }
 
   h1 {
     font-family: 'Walter Turncoat';
     margin: 0 auto;
+    width: 95%;
     max-width: 50rem;
     font-size: 4rem;
     line-height: 1.2;
     margin-bottom:2rem;
-    color:rgba(0,0,0,.8)
+    color:rgba(0,0,0,.8);
+  }
+
+  @media (max-width: 600px){
+    h1 {
+      font-size: 3rem;
+    }
+  }
+
+  @media (max-width: 400px){
+    h1 {
+      font-size: 2.5rem;
+    }
+  }
+
+  @media (max-width: 330px){
+    h1 {
+      font-size: 2rem;
+    }
   }
 
   .intro {
@@ -50,9 +72,12 @@
   }
 
   p {
-    color: black;
+    color:rgba(0,0,0,.8);
     font-family: 'Lyon Text Web';
     font-size:1.3rem;
+    width: 95%;
+    margin: 0 auto;
+    margin-bottom: 1.5rem;
   }
 
   .byline p{
@@ -64,6 +89,19 @@
   a:hover{
     border-bottom:1px solid black;
   }
+
+  ul li {
+    font-family: 'Lyon Text Web';
+    font-size: 1.3rem;
+    color:rgba(0,0,0,.8);
+    margin-bottom: 1.3rem;
+    line-height:1.65;
+    margin-left: 2rem;
+    list-style-type: disc;
+    padding-left: 1rem;
+  }
+
+
 </style>
 
 <script>
@@ -82,4 +120,9 @@
   {#each doc.intro as intro}
     <p class='prose'>{@html intro.value}</p>
   {/each}
+  <ul>
+    {#each doc.outro as outro}
+      <li class='prose bullet'>{@html outro.value}</li>
+    {/each}
+  </ul>
 </div>
