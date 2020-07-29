718267b43f8ee1fb539c2581eb5fa6e70283a0c0
Matt Daniels <matthew.f.daniels@gmail.com>
2020-07-29 11:13:01 -0400

    update github pages

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -4,12 +4,12 @@
 
 <meta property="og:title" content="The Courage (and Disappointment) of Pitching a Visual Essay" />
 <meta property="og:site_name" content="The Pudding" />
-<meta property="og:url" content="https://pudding.cool/2020/07/pitching-gendered-descriptions" />
+<meta property="og:url" content="https://pudding.cool/process/pitching-gendered-descriptions" />
 <meta property="og:description" content="How I successfully pitched my first visual essay" />
 <meta property="og:type" content="article" />
 <meta property="og:locale" content="en_US" />
 
-<meta property="og:image" content="https://pudding.cool/2020/07/pitching-gendered-descriptions/assets/fb.jpg" />
+<meta property="og:image" content="https://pudding.cool/process/pitching-gendered-descriptions/assets/fb.jpg" />
 <meta property="og:image:type" content="image/jpeg" />
 <meta property="og:image:width" content="1200" />
 <meta property="og:image:height" content="630" />
@@ -19,9 +19,9 @@
 <meta name="twitter:creator" content="@erindataviz" />
 <meta name="twitter:title" content="The Courage (and Disappointment) of Pitching a Visual Essay" />
 <meta name="twitter:description" content="How I successfully pitched my first visual essay" />
-<meta name="twitter:image:src" content="https://pudding.cool/2020/07/pitching-gendered-descriptions/assets/tw.jpg" />
+<meta name="twitter:image:src" content="https://pudding.cool/process/pitching-gendered-descriptions/assets/tw.jpg" />
 
-<link rel="canonical" href="https://pudding.cool/2020/07/pitching-gendered-descriptions" />
+<link rel="canonical" href="https://pudding.cool/process/pitching-gendered-descriptions" />
 
 <link rel='preload' type='font/woff2' as='font' crossorigin href='assets/fonts/lyon/LyonText-Regular-Web.woff2'>
 <link rel='preload' type='font/woff2' as='font' crossorigin href='assets/fonts/lyon/LyonDisplay-Regular-Web.woff2'>
